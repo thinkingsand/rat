be362144fe7991cec3f230abcf2de3da55a6b00d
Matthew Charlston <matthew@mcharlsto.uk>
2023-01-12 21:06:38 +0000

    Make better

--- script.sh
+++ script.sh
@@ -5,18 +5,24 @@ trap ctrl_c INT
 ctrl_c () {
         rm *.txt
         rm *.sh
+        killall play
         exit 0
 }
 
+spins=0
+play audio.opus repeat 9999 </dev/null &>/dev/null &
 clear
 while true
 do
 i=1
 while [ $i -ne 31 ]
         do
+                echo
+                echo "                                                          You have been blessed with $spins spins of the rat."
                 i=$(($i+1))
                 cat $i.txt
                 sleep 0.1
                 clear
         done
+spins=$(($spins+1))
 done
