6e3b8f13a69f0d12ddf19a150a612bc335c24328
IAGOnDiscord <discorduseriamgod@gmail.com>
2023-01-17 14:12:04 -0500

    Repair damaage on window resize

--- script.sh
+++ script.sh
@@ -4,30 +4,40 @@ function startup {
     printf '\e[?25l'    # hide cursor
     printf '\e[?7l'     # disable line wrap
     clear
-    play -q audio.opus repeat 9999999 </dev/null &>/dev/null & # loop audio playback basically forever
-    pid=!
+    play -q audio.opus repeat 9999999 </dev/null &>/dev/null & # play audio
 }
-
 function wrapup {
     printf '\e[?25h'    # show cursor
     printf '\e[?7h'     # enable line wrap
     clear
-    kill $pid
 }
 trap wrapup EXIT;
 
+
+function drawBar {
+    printf '\e[2;58H'   # move cursor to line 2, col 58
+    echo "You have been blessed with $spins spins of the rat."
+}
+function drawArt {
+    printf '\e[3H'      # move cursor to line 3
+    cat $index.txt
+}
+
+function fixDamage {
+    clear
+    drawBar
+    drawArt
+}
+trap fixDamage WINCH    # activate on window resize
+
 startup
 spins=0
-while true
-do
-    printf '\e[2H'      # move cursor to line 2
-    echo "                                                          You have been blessed with $spins spins of the rat."
-    i=1
-    while [ $i -le 31 ]
-    do
-        cat $i.txt
-        i=$((i+1));
-        printf '\e[3H'  # move cursor to line 3
+while true; do
+    drawBar
+    index=1
+    while [ $index -le 31 ]; do
+        drawArt
+        index=$((index+1))
         sleep 0.1
     done
     spins=$(($spins+1));
