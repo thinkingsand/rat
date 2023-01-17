07f6678ced24aefc72deb3c4b1fc93b38069e616
IAGOnDiscord <discorduseriamgod@gmail.com>
2023-01-17 12:50:47 -0500

    Complete rewrite of script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
+function startup {
+    printf '\e[?25l'    # hide cursor
+    printf '\e[?7l'     # disable line wrap
+    clear
+    play -q audio.opus repeat 9999999 </dev/null &>/dev/null & # loop audio playback basically forever
+    pid=!
+}
+
+function wrapup {
+    printf '\e[?25h'    # show cursor
+    printf '\e[?7h'     # enable line wrap
+    clear
+    kill $pid
+}
+trap wrapup EXIT;
+
+startup
 spins=0
-play -q audio.opus repeat 9999 </dev/null &>/dev/null &
-clear
 while true
 do
-i=1
-while [ $i -le 30 ]
-        do
-                echo
-                echo "                                                          You have been blessed with $spins spins of the rat."
-                i=$(($i+1))
-                cat $i.txt
-                sleep 0.1
-                clear
-        done
-spins=$(($spins+1))
+    printf '\e[2H'      # move cursor to line 2
+    echo "                                                          You have been blessed with $spins spins of the rat."
+    i=1
+    while [ $i -le 31 ]
+    do
+        cat $i.txt
+        i=$((i+1));
+        printf '\e[3H'  # move cursor to line 3
+        sleep 0.1
+    done
+    spins=$(($spins+1));
 done
